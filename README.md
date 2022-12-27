662d73cd41ee4559e8320226be8ef421fca609ff
Rajiv Ramakrishnan <rajiv270992@gmail.com>
2022-12-27 15:55:28 +0530

    feat: deploying dao in goerli

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # crypto-dev-dao
+
+## Crypto Devs DAO
+
+[Fake NFT Marketplace Contract Address](https://goerli.etherscan.io/address/0x0f2171F2E62Dbf81f5f9f8507ac70B73E8054954)
+
+[Crypto Dev DAO Contract Address](https://goerli.etherscan.io/address/0xC6f8ff9d46d1E686Cff7Fa719C2ed479C87fC1eA)
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,23 +1,38 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+import { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } from '../constants';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Deploying the fake NFT marketplace.
+  const fakeNFTMarketPlaceContract = await ethers.getContractFactory(
+    'FakeNFTMarketplace'
+  );
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const deployedFakeNFTMPContract = await fakeNFTMarketPlaceContract.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log('Fake NFT Contract Address:', deployedFakeNFTMPContract.address);
 
-  await lock.deployed();
+  // Address of the Crypto Devs NFT contract that you deployed in the previous module
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const cryptoDevDaoContract = await ethers.getContractFactory('CryptoDevsDAO');
+
+  const deployedCryptoDevsTokenContract = await cryptoDevDaoContract.deploy(
+    deployedFakeNFTMPContract.address,
+    cryptoDevsNFTContract,
+    {
+      value: ethers.utils.parseEther('0.01'),
+    }
+  );
+
+  console.log(
+    'Crypto Devs DAO Contract Address:',
+    deployedCryptoDevsTokenContract.address
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
